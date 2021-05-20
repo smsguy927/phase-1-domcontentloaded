a60af95b4187139ca3fa194d7c2220371ffac0ba
smsguy927 <smsguy927@gmail.com>
2021-05-20 14:47:29 -0500

    commit tree aae48099429e06e91e245a1e5f5df2ed05e7eb54

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+let p = document.querySelector('p');
+let listener = document.addEventListener("DOMContentLoaded", function() {
+  p.textContent = "This is really cool!"  
+});
+
